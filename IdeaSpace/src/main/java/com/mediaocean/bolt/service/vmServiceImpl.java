fb1ca55933fec6e93be06a725cd20d062714e022
shubhamb98 <shubham.bhavsar@gmail.com>
2019-11-11 15:26:40 +0530

    Added code for start and stop vm.

--- IdeaSpace/src/main/java/com/mediaocean/bolt/service/vmServiceImpl.java
+++ IdeaSpace/src/main/java/com/mediaocean/bolt/service/vmServiceImpl.java
@@ -2,6 +2,8 @@ package com.mediaocean.bolt.service;
 
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service
 public class vmServiceImpl {
 
@@ -12,10 +14,40 @@ public class vmServiceImpl {
     public String stopAllVMs(){
      return "All VMs stoppped";
     }
-    public String startVM(){
+    public String startVM(String node_name)
+    {
+        ProcessBuilder builder = new ProcessBuilder(    
+                "cmd.exe", "/C", "cd \"C:\\PSTools\" && PsExec.exe \\\\"+node_name +" -accepteula -u DDS-NA\\qaadmin -p qatest -s -i 2 -d \\\\ny-vpc-sgrid.na.rtdom.net\\GRID_BINARIES\\starttemp.bat");
+
+        builder.redirectInput(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectError(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        Process p = null;
+        try {
+            p = builder.start();
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
      return "VM started";
     }
-    public String stopVM(){
+    public String stopVM(String node_name){
+
+        ProcessBuilder builder = new ProcessBuilder(
+                "cmd.exe", "/C", "cd \"C:\\PSTools\" && PsExec.exe \\\\"+node_name +"-accepteula -s -u DDS-NA\\qaadmin -p qatest cmd /c \\\\ny-vpc-sgrid.na.rtdom.net\\GRID_BINARIES\\StopNode.bat");
+
+        builder.redirectInput(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectError(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        Process p = null;
+        try {
+            p = builder.start();
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
      return "VM stopped";
     }
 }
