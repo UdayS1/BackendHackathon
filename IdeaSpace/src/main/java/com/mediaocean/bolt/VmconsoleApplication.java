19a4b9e8e34e46da697badb8bb9777e755180e50
usawant <usawant@mediaocean.com>
2019-11-11 14:42:53 +0530

    initial commit

---
+++ IdeaSpace/src/main/java/com/mediaocean/bolt/VmconsoleApplication.java
@@ -0,0 +1,13 @@
+package com.mediaocean.bolt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmconsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VmconsoleApplication.class, args);
+    }
+
+}
