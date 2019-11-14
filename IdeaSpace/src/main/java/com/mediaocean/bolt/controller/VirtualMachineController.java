8f7b78993f80ca531248dd9efde346849c4b59a3
usawant <usawant@mediaocean.com>
2019-11-14 19:14:09 +0530

    Base model for hackathon

--- IdeaSpace/src/main/java/com/mediaocean/bolt/controller/VirtualMachineController.java
+++ IdeaSpace/src/main/java/com/mediaocean/bolt/controller/VirtualMachineController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+
 @RestController
 @CrossOrigin
 @RequestMapping(value = { "/vm" })
@@ -19,14 +22,28 @@ public class VirtualMachineController {
     @Autowired
     private vmServiceImpl service;
 
+    @GetMapping(value = "/instanceData")
+    public List<String> initialInformation(){
+        return service.instanceData();
+    }
     @GetMapping(value = "/start")
-    public String startVMs(@RequestParam String nodeName) {
+    public String startVM(@RequestParam String nodeName) {
         return service.startVM(nodeName);
     }
 
+    @GetMapping(value = "/restart")
+    public String restartVM(@RequestParam String nodeName) {
+        return service.restartVm(nodeName);
+    }
+
     @GetMapping(value = "/stop")
-    public String stopVMs(@RequestParam String nodeName) {
+    public String stopVM(@RequestParam String nodeName) {
         return service.stopVM(nodeName);
+    }
+
+    @GetMapping(value = "/cleanMemory")
+    public String cleanMemoryVM(@RequestParam String nodeName) {
+        return service.cleanMemoryVM(nodeName);
     }
 
     @GetMapping(value = "/startAll")
--- IdeaSpace/src/main/java/com/mediaocean/bolt/service/vmServiceImpl.java
+++ IdeaSpace/src/main/java/com/mediaocean/bolt/service/vmServiceImpl.java
@@ -1,11 +1,21 @@
 package com.mediaocean.bolt.service;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class vmServiceImpl {
+
 String all_vms="NY-VPC-PRIS02,NY-VPC-PRIS04,NY-VPC-PRIS07,NY-VPC-PRIS08,NY-VPC-PRIS09,NY-VPC-PRIS10,NY-VPC-PRIS11,NY-VPC-PRIS12,NY-VPC-PRIS13,NY-VPC-PRIS14,NY-VPC-PRIS15,NY-VPC-PRIS16,NY-VPC-PRIS17,NY-VPC-PRIS18,NY-VPC-PRIS23,NY-VPC-PRIS24,NY-VPC-PRIS25,NY-VPC-PRIS26,NY-VPC-PRIS27";
     public String startAllVMs(){
       /*  ProcessBuilder builder = new ProcessBuilder(
@@ -58,14 +68,67 @@ String all_vms="NY-VPC-PRIS02,NY-VPC-PRIS04,NY-VPC-PRIS07,NY-VPC-PRIS08,NY-VPC-P
             return "VM started";
         } catch (Exception e) {
             e.printStackTrace();
-            return "VM not started";
+            return "STARTED";
         }
 
+    }
+
+    public List<String> instanceData(){
+        List<String> active_vms=new ArrayList<>();
+         List<WebElement> No_Instances;
+        String[] Vms = {"NY-VPC-PRIS25", "NY-VPC-PRIS09",};
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\usawant\\Downloads\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+
+
+        //ChromeDriver chromeDriver = new ChromeDriver(options);
+        WebDriver d = new ChromeDriver(options);
+        d.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        d.manage().window().maximize();
+        d.get("http://ny-vpc-pris03.na.rtdom.net:4494/grid/console");
+        d.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+
+
+        List<WebElement> active = d.findElements(By.xpath("//p[@class=\"proxyid\"]"));
+
+        int i = 0;
+
+        for (i = 0; i < active.size(); i++) {
+
+            String temp = active.get(i).getText();
+            active_vms.add(temp.substring(12, 25));
+            System.out.println(temp);
+        }
+
+        return active_vms;
+//        int j = 0;
+//        HashMap<String, Integer> Busy = new HashMap<>();
+//        String busy = "//p[text()='id : http://%s:5568, OS : VISTA'] /parent::div//img[@class='busy']";
+//        String sss;
+//        int count=0;
+//        System.out.println(active_vms.size());
+//        for (int k=0;k<active_vms.size();k++){
+//            count=0;
+//            try {
+//                sss = busy.replace("%s", active_vms.get(k));
+//                if (d.findElements(By.xpath(sss)).size() != 0) {
+//                    No_Instances = d.findElements(By.xpath(sss));
+//                    count=No_Instances.size();
+//                }
+//                Busy.put(active_vms.get(k), count);
+//                System.out.println("Name" + active_vms.get(k) + "No of Active Instances :" + count);
+//            } catch (Exception e) {
+//                System.out.println(e);
+//            }
+//        }
+//        return Busy;
     }
     public String stopVM(String node_name){
 
         ProcessBuilder builder = new ProcessBuilder(
-                "cmd.exe", "/C", "cd \"C:\\PSTools\" && PsExec.exe \\\\"+node_name +"-accepteula -s -u DDS-NA\\qaadmin -p qatest cmd /c \\\\ny-vpc-sgrid.na.rtdom.net\\GRID_BINARIES\\StopNode.bat");
+                "cmd.exe", "/C", "cd \"C:\\PSTools\" && PsExec.exe \\\\"+node_name+" -accepteula -s -u DDS-NA\\qaadmin -p qatest cmd /c \\\\ny-vpc-sgrid.na.rtdom.net\\GRID_BINARIES\\StopNode.bat");
 
         builder.redirectInput(ProcessBuilder.Redirect.INHERIT);
         builder.redirectError(ProcessBuilder.Redirect.INHERIT);
@@ -79,6 +142,46 @@ String all_vms="NY-VPC-PRIS02,NY-VPC-PRIS04,NY-VPC-PRIS07,NY-VPC-PRIS08,NY-VPC-P
         }
 
 
-     return "VM stopped";
+     return "STOPPED";
+    }
+    public String cleanMemoryVM(String node_name){
+
+        ProcessBuilder builder = new ProcessBuilder(
+                "cmd.exe", "/C", "cd \"C:\\PSTools\" && PsExec.exe \\\\"+node_name+" -accepteula -s -u DDS-NA\\qaadmin -p qatest cmd /c \\\\ny-vpc-sgrid.na.rtdom.net\\GRID_BINARIES\\cleanup.bat");
+
+        builder.redirectInput(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectError(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        Process p = null;
+        try {
+            p = builder.start();
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return "VM memory cleared";
+    }
+
+    public String restartVm(String node_name){
+        ProcessBuilder builder = new ProcessBuilder(
+                "cmd.exe", "/C", "cd \"C:\\PSTools\" && PsExec.exe \\\\"+node_name+" -accepteula -u DDS-NA\\qaadmin -p qatest -s -i 2 -d \\\\ny-vpc-sgrid.na.rtdom.net\\GRID_BINARIES\\restart.bat");
+
+
+        builder.redirectInput(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectError(ProcessBuilder.Redirect.INHERIT);
+        builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
+        Process p = null;
+        try {
+            p = builder.start();
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return "VM memory cleared";
+
     }
 }
